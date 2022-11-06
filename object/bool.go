3fad0eb26239735ebc11b5297faf06b4983a8588
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-11-06 19:41:11 +0100

    Fix Boolean object Type() returning INTEGER_OBJ

--- object/bool.go
+++ object/bool.go
@@ -6,5 +6,5 @@ type Boolean struct {
 	Value bool
 }
 
-func (b *Boolean) Type() ObjectType { return INTEGER_OBJ }
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
