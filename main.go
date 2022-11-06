7803584cbca08e3b90c029687b1ea84cdf94677b
Marcin Paliwoda <paliwoda.marcin@zoho.com>
2022-11-06 17:52:30 +0100

    Writing an interpreter in Go part 7

--- main.go
+++ main.go
@@ -8,14 +8,27 @@ import (
 	"github.com/mpaliwoda/interpreter-book/repl"
 )
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func main() {
 	user, err := user.Current()
 
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("Hello %s! This is the Monkey programming language.\n", user.Username)
-	fmt.Printf("Feel free to type in commands!\n")
+	fmt.Print(MONKEY_FACE)
+	fmt.Printf("\nHello %s! This is the Monkey programming language.\n", user.Username)
+	fmt.Printf("Feel free to type in commands! To exit, type in: .exit\n\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -7,10 +7,11 @@ import (
 	"os"
 
 	"github.com/mpaliwoda/interpreter-book/lexer"
-	"github.com/mpaliwoda/interpreter-book/token"
+	"github.com/mpaliwoda/interpreter-book/parser"
 )
 
 const PROMPT = "$> "
+
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -24,15 +25,28 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.New(line)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			if tok.Literal == "exit" {
-				fmt.Printf("Bye!\n")
-				os.Exit(0)
-			}
-
-			fmt.Fprintf(out, "%+v\n", tok)
+		if line == ".exit" {
+			fmt.Println("Bye!")
+			os.Exit(0)
 		}
+
+		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
